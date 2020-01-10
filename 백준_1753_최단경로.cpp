68afe08619840f07471e66698246281866aecaa1
yglee8048 <yglee8048@gmail.com>
2020-01-10 15:50:07 +0900

    최단경로... 알고리즘 최적화 및 주석 수정 필요

--- 백준_1753_최단경로.cpp
+++ 백준_1753_최단경로.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -7,9 +9,19 @@ const int INF = 9876543210;
 const int SIZE = 20001;
 int v, e, k;                        // v : 정점의 개수, e : 간선의 개수, k : 시작 정점의 번호
 int dis[SIZE];                      // 정점 k에서 idx 까지의 거리
-vector<pair<int, int>> graph[SIZE]; // graph[a] = (b, w), a에서 b까지의 가중치 w의 간선
+vector<pair<int, int>> graph[SIZE]; // graph[a] = (b, w) : a에서 b까지의 가중치 w의 간선
+priority_queue<pair<int, int>> pq;  // pair(d, node) : node 까지의 거리 d
 
-int dfs(int now, int sum)
+bool comp(const pair<int, int> &a, const pair<int, int> &b)
+{
+  if (a.second == b.second)
+  {
+    return a.first < b.first;
+  }
+  return a.second < b.second;
+}
+
+void dfs(int now, int sum)
 {
   for (int i = 0; i < graph[now].size(); i++)
   {
@@ -18,7 +30,40 @@ int dfs(int now, int sum)
 
     if (dis[nxt] > sum + weight)
     {
+      dis[nxt] = sum + weight;
       dfs(nxt, sum + weight);
+    }
+  }
+}
+
+void bfs(int start)
+{
+  // init
+  dis[start] = 0;
+  pq.push(make_pair(0, start));
+
+  // bfs
+  while (!pq.empty())
+  {
+    int now = pq.top().second;
+    int distance = -pq.top().first;
+    pq.pop();
+
+    if (dis[now] < distance)
+    {
+      continue;
+    }
+
+    for (int i = 0; i < graph[now].size(); i++)
+    {
+      int nxt = graph[now][i].first;
+      int weight = graph[now][i].second;
+
+      if (dis[nxt] > distance + weight)
+      {
+        dis[nxt] = distance + weight;
+        pq.push(make_pair(-1 * (distance + weight), nxt));
+      }
     }
   }
 }
@@ -28,20 +73,27 @@ int main()
   // 입력
   cin >> v >> e;
   cin >> k;
-  for (int i = 0; i < v; i++)
+  for (int i = 0; i < e; i++)
   {
     int a, b, w;
     cin >> a >> b >> w;
 
     graph[a].push_back(make_pair(b, w));
-    graph[b].push_back(make_pair(a, w));
   }
 
-  for (int i = 0; i < v; i++)
+  for (int i = 1; i <= v; i++)
   {
+    sort(graph[i].begin(), graph[i].end(), comp);
     dis[i] = INF;
   }
-  dis[k] = 0;
+
+  // dfs(k, 0);
+  bfs(k);
+
+  for (int i = 1; i <= v; i++)
+  {
+    dis[i] == INF ? cout << "INF" << '\n' : cout << dis[i] << '\n';
+  }
 
   return 0;
 }
