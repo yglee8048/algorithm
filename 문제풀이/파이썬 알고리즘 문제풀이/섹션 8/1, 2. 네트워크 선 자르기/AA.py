5b57f3e41b3c74f609a14164f516f20741275976
yglee8048 <yglee8048@gmail.com>
2020-05-22 07:16:17 +0900

    8-1,2 복습

--- 문제풀이/파이썬 알고리즘 문제풀이/섹션 8/1, 2. 네트워크 선 자르기/AA.py
+++ 문제풀이/파이썬 알고리즘 문제풀이/섹션 8/1, 2. 네트워크 선 자르기/AA.py
@@ -1,13 +1,42 @@
 import sys
 # sys.stdin = open('input.txt', 'rt')
 
-if __name__ == "__main__":
-    n = int(input())
-    line = [0] * (n+1)
+
+def init():
+    global n, line
+
+    line = [-1] * (n+1)
     line[0] = 0
     line[1] = 1
     line[2] = 2
 
+
+def bottom_up():
+    global n, line
+    init()
+
     for i in range(3, n+1):
         line[i] = line[i-1] + line[i-2]
-    print(line[n])
+    return line[n]
+
+
+def dfs(x):
+    global line
+    if line[x] < 0:
+        line[x] = dfs(x-1) + dfs(x-2)
+    return line[x]
+
+
+def top_down():
+    global n, line
+    init()
+
+    return dfs(n)
+
+
+if __name__ == "__main__":
+    n = int(input())
+    line = [-1] * (n+1)
+
+    # print(bottom_up())
+    print(top_down())
