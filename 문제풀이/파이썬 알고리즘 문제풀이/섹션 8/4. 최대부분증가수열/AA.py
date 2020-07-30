e31a0299c5c3162e60e2d2cedd6603a628566a35
yglee8048 <yglee8048@gmail.com>
2020-07-31 00:22:43 +0900

    섹션 8 / 4. 최대부분증가수열

--- 문제풀이/파이썬 알고리즘 문제풀이/섹션 8/4. 최대부분증가수열/AA.py
+++ 문제풀이/파이썬 알고리즘 문제풀이/섹션 8/4. 최대부분증가수열/AA.py
@@ -1,11 +1,11 @@
 import sys
-sys.stdin = open('input.txt', 'rt')
+# sys.stdin = open('input.txt', 'rt')
 
 if __name__ == "__main__":
     n = int(input())
     arr = list(map(int, input().split()))
 
-    dp = [0] * n
+    dp = [0] * (n+1)
     dp[0] = 1
     for i in range(1, n):
         p_max = 0
